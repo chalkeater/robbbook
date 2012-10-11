d2bf3353f9dcd779e6d9dd6dda22ed02a6277869
Robb Schuneman <robbschuneman@gmail.com>
2012-10-11 15:10:27 -0500

    Added status validations and tests

--- test/unit/helpers/statuses_helper_test.rb
+++ test/unit/helpers/statuses_helper_test.rb
@@ -1,4 +1,9 @@
 require 'test_helper'
 
 class StatusesHelperTest < ActionView::TestCase
+	test "that a status requires content" do 
+		status = Status.new
+		assert !status.save
+		assert !status.errors[:content].empty
+	end
 end
